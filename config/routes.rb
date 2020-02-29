36f18c93b59ac758529b76fa3a191c4b29b92717
Alvaro Alvarez Arrieta <ajalvarez@MacBook-Pro-de-Alvaro.local>
2020-02-29 13:09:10 -0500

    setting devise for user model

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,22 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+#   scope :api, defaults: { format: :json } do
+#     scope :v1 do
+#       devise_for :users, controllers: { sessions: 'api/v1/users/sessions' }
+#     end
+#   end
+# end
+  # devise_for :users, controllers: { sessions: 'api/v1/users/sessions',
+  #                                   registrations: 'api/v1/users/registrations' }
+  devise_for :users
+
+  scope :api, defaults: { format: :json } do
+    scope :v1 do
+      get 'users', to: 'api/v1/users#show'
+
+      devise_scope :user do
+        match '/sign_up', to: 'api/v1/users/registrations#create', via: :post
+        match '/sign_in', to: 'api/v1/users/sessions#create', via: :post
+      end
+    end 
+  end
 end
